2e4c0b4056d4cbdca198e3a94ebfe9cc217f2712
HelloTC <taicheng_huang@mail.bnu.edu.cn>
2021-10-20 09:45:37 +0800

    delete load_bullet function

--- utils/utils.py
+++ utils/utils.py
@@ -9,19 +9,6 @@ const = macro_const.Const()
 def print_separator(n=50):
     print('\n' + n*'-' + '\n')
 
-
-# Pybullet Model
-def load_pybullet(filename, fixed_base=False):
-    if filename.endswith('.urdf'):
-        flags = get_urdf_flags(**kwargs)
-        body = p.loadURDF(filename, useFixedBase=fixed_base, flags=flags,
-                            globalScaling=scale, physicsClientId=CLIENT)
-    elif filename.endswith('.obj'):
-        body = create_obj(filename, scale=scale, **kwargs)
-    else:
-        raise ValueError(filename)
-    return body
-
 # Rendering Setting
 def disable_rendering():
     p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
