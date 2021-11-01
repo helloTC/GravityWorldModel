6ee7b65d05cd7ac93dffca14643bd449e195db85
HelloTC <taicheng_huang@mail.bnu.edu.cn>
2021-11-01 11:26:43 +0800

    provide interface to adjust camera vision

--- utils/utils.py
+++ utils/utils.py
@@ -21,7 +21,7 @@ def disable_rendering():
 # Rotation Transformation
 
 # Camera
-def set_camera(distance, yaw, pitch, height):
+def set_camera(distance, yaw, pitch, height, x_offset=0, y_offset=0):
     """
     Set camera position.
     Note that for simplicity, target position of the camera was fixed and followed the center of objects.
@@ -29,8 +29,10 @@ def set_camera(distance, yaw, pitch, height):
     distance[float]: camera distance.
     yaw[float]: camera yaw angle (in degrees, left/right).
     pitch[float]: camera pitch angle (in degrees, up/down).
-    height[float]: camera height. 
+    height[float]: camera height.
+    x_offset[float]: offset to modify x coordinate. By default is 0.
+    y_offset[float]: offset to modify y coordinate. By default is 0. 
     """
-    x = distance*np.cos(yaw*const.PI/180)
-    y = distance*np.sin(yaw*const.PI/180)
+    x = distance*np.cos(yaw*const.PI/180) + x_offset
+    y = distance*np.sin(yaw*const.PI/180) + y_offset
     p.resetDebugVisualizerCamera(distance, yaw+90, pitch, [x,y,height]) 
